a0ea3f8938fa1079558f20d8697e81bf34f06fef
Tanishka Gupta <tashiaevans05@gmail.com>
2023-05-25 00:09:41 +0530

    second commit

--- code_together/core/views.py
+++ code_together/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import ActiveUser, RoomGroup
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+def room(request, room_id, username):
+    context = {
+        "room_id": room_id,
+        "username": username
+    }
+    # try:
+        # room_group = RoomGroup.objects.get(room_id=room_id)
+        # active_users = ActiveUser.objects.filter(room_group=room_group)
+    #     context['active_users']
+    return render(request, 'room.html', context)
